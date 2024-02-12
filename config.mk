894c208ed0608293263988f0f3e0ca077d5f4e47
Øyvind Harboe <oyvind.harboe@zylin.com>
2024-02-12 07:30:51 +0100

    OpenROAD-flow-scripts: basic scripts to start investigating
    1. Clone ORFS to https://github.com/The-OpenROAD-Project/OpenROAD-flow-scripts
    2. Build https://github.com/The-OpenROAD-Project/OpenROAD-flow-scripts/blob/master/docs/user/BuildLocally.md
    
    Run:
    
    $ ./orfs make YOSYS_FLAGS= DESIGN_CONFIG=config.mk synth
    [deleted]
    Creating register for signal `\TOP_MODULE.\resultSrcE' using process `\TOP_MODULE.$proc$Codes/RTL/TOP_MODULE.v:61$10'.
      created $dff cERROR: Multiple edge sensitive events found for this signal!
    Command exited with non-zero status 1
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

---
+++ config.mk
@@ -0,0 +1,15 @@
+export DESIGN_NAME            = TOP_MODULE
+export DESIGN_NICKNAME        = riscv
+
+export VERILOG_FILES          = Codes/RTL/*.v
+
+export SDC_FILE               = constraint.sdc
+
+export PLATFORM               = asap7
+export PLACE_DENSITY          = 0.30
+export CORE_UTILIZATION       = 0.50
+
+export GDS_ALLOW_EMPTY       = Element
+
+export GND_NETS_VOLTAGES      =
+export PWR_NETS_VOLTAGES      =
